302090c83363cb009788bf4cb4d6c95e10b028eb
= <=>
2022-11-28 17:00:36 +0530

    Updating Readme

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# To load the variables in the .env file
+To load the variables in the .env file : 
 source .env
 
-# To deploy and verify our contract
+To deploy and verify our contract:
 forge script script/Deploy.s.sol:DeployScript --rpc-url $GOERLI_RPC_URL --broadcast --verify -vvvv
 
+Contract Deployed Address:
 Deployed COntract : https://api-goerli.etherscan.io/apiaddress/0xea068f2d97280a2457670fbe7d8adfe82665db4b
--- src/ITimeLockedWallet.sol
+++ src/ITimeLockedWallet.sol
@@ -16,18 +16,3 @@ interface IWallet{
     function claimTokens(address _ERC20, uint256 _amount) external  returns(bool success);
 
 }
-import { Biconomy } from "@biconomy/mexa";
-
-  const biconomy = new Biconomy(window.ethereum, {
-    apiKey: config.apikey,
-    debug: true,
-    contractAddresses: [config.factoryAddress], // list of contract address you want to enable gasless on
-  });
-
-      const contract = new ethers.Contract(
-      factoryAddress,
-      Factory.abi,
-      biconomy.ethersProvider
-    );
-
-      await biconomy.init();
